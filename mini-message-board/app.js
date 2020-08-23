763d61304657774f81d88a91d6f7b9561ba6244c
foolmonkey <foolmonkey99@gmail.com>
2020-08-23 15:32:53 -0600

    Display static messages list to view with styling

--- mini-message-board/app.js
+++ mini-message-board/app.js
@@ -4,15 +4,31 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var exphbs = require("express-handlebars");
+var datefns = require("date-fns");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 var newRouter = require("./routes/new");
 
 var app = express();
 
+// handlebars helper functions
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  layoutsDir: path.join(__dirname, "views/layouts"),
+
+  // helpers
+  helpers: {
+    dateformat: function (aDate) {
+      return datefns.formatDistanceToNow(aDate, {
+        includeSeconds: true,
+        addSuffix: true,
+      });
+    },
+  },
+});
+
 // view engine setup
-app.engine("handlebars", exphbs());
+app.engine("handlebars", hbs.engine);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "handlebars");
 
@@ -23,7 +39,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/new", newRouter);
 
 // catch 404 and forward to error handler
